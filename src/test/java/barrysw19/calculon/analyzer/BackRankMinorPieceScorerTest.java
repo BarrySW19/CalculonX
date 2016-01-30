52f5cb94d992afc9fb8c99a2681d60381c9e085b
BarrySW19 <barrysw19@gmail.com>
2016-01-30 00:12:11 +0000

    test fixes

--- src/test/java/barrysw19/calculon/analyzer/BackRankMinorPieceScorerTest.java
+++ src/test/java/barrysw19/calculon/analyzer/BackRankMinorPieceScorerTest.java
@@ -12,19 +12,19 @@ public class BackRankMinorPieceScorerTest extends AbstractAnalyserTest {
 
     @Test
     public void testScore1() {
-        setPosition("1k6/8/8/8/8/8/8/1B4NK b - - 0 1");
+        setPosition("1k6/8/8/7q/7Q/8/8/1B4NK b - - 0 1");
         assertEquals(-300, scorer.scorePosition(board, context));
     }
 
     @Test
     public void testScore2() {
-        setPosition("1kn5/8/8/8/8/8/8/1B4NK b - - 0 1");
+        setPosition("1kn5/8/8/7q/7Q/8/8/1B4NK b - - 0 1");
         assertEquals(-150, scorer.scorePosition(board, context));
     }
 
     @Test
     public void testScore3() {
-        setPosition("1k6/8/8/8/8/8/B7/6NK b - - 0 1");
+        setPosition("1k6/8/8/7q/7Q/8/B7/6NK b - - 0 1");
         assertEquals(-150, scorer.scorePosition(board, context));
     }
 }
