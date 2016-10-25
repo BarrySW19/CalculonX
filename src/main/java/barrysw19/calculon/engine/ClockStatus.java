d8346c845c3dc928173f0eb0158b505e5924ea9f
BarrySW19 <barrysw19@gmail.com>
2016-10-25 19:37:09 +0100

    adding time handling to FICS interface

--- src/main/java/barrysw19/calculon/engine/ClockStatus.java
+++ src/main/java/barrysw19/calculon/engine/ClockStatus.java
@@ -5,31 +5,44 @@ public class ClockStatus {
     private int secondIncrement;
     private long msec;
 
+    public ClockStatus() { }
+
+    public ClockStatus(int initialMinutes, int secondIncrement) {
+        setInitialMinutes(initialMinutes);
+        setSecondIncrement(secondIncrement);
+    }
+
     public int getInitialMinutes() {
         return initialMinutes;
     }
 
-    public void setInitialMinutes(int initialMinutes) {
+    public ClockStatus setInitialMinutes(int initialMinutes) {
         this.initialMinutes = initialMinutes;
         msec = initialMinutes * 60 * 1000;
+        return this;
     }
 
     public int getSecondIncrement() {
         return secondIncrement;
     }
 
-    public void setSecondIncrement(int secondIncrement) {
+    public ClockStatus setSecondIncrement(int secondIncrement) {
         this.secondIncrement = secondIncrement;
+        return this;
     }
 
     public long getMsec() {
         return msec;
     }
 
-    public void setMsec(long msec) {
+    public ClockStatus setMsec(long msec) {
         this.msec = msec;
+        return this;
     }
 
+    /**
+     * Return an estimate of the time available for the next n moves.
+     */
     public int getSecondsForMoves(int moves) {
         int sec = (int) (this.msec / 1000);
         sec += (moves * secondIncrement);
